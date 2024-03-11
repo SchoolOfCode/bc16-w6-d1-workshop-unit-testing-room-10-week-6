f1ee291708aa02ff5835ac9e809beb390538e728
teonkajkafon <catherinesarquis@gmail.com>
2024-03-11 12:31:17 +0000

    save before lunch

---
+++ proof-of-concept/sum.test.js
@@ -0,0 +1,12 @@
+import { sum } from "/sum.js";
+import { expect, test } from "vitest";
+
+//  import `test` and `expect` functions from Vitest ✅
+//  import the `sum` function from sum.js
+//  create a bare bones test
+test("bare bones test should pass", () => {});
+
+//  run Vitest to check that it detects and passes the new test
+//  inside the test:
+//      call the sum function with arguments: 5 and 7
+//      assert that the value returned is 12
